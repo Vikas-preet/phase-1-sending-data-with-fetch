e56a21a98d204d96a15279b20c49420e441a2d77
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-05-21 16:26:00 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+// Test-1: Send data
+
+const configrationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com",
+  }),
+};
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", configrationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (Object) {
+      document.body.textContent = Object["id"];
+    })
+    .catch(function (error) {
+      alert("something went wrong");
+      console.log(error.message);
+      document.body.textContent = error.message;
+    });
+}
+
+submitData();
